7541c340f122861f570ff094fc8f27da7be47dd2
bekirov <vbekirovv@gmail.com>
2022-09-19 23:23:31 +0300

    Implemented db context.

---
+++ GameStore.Infrastructure/Persistence/GameStoreContext.cs
@@ -0,0 +1,22 @@
+﻿using GameStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Infrastructure.Persistence;
+
+public class GameStoreContext : DbContext
+{
+    public GameStoreContext(DbContextOptions<GameStoreContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Game> Games { get; set; }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Genre> Genres { get; set; }
+
+    public DbSet<PlatformType> Platforms { get; set; }
+
+    public DbSet<Comment> Comments { get; set; }
+}
