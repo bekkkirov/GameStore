31af81328d509cbb48fca08e19d74b0381992eb3
bekirov <vbekirovv@gmail.com>
2022-09-22 12:41:19 +0300

    Added validators.

---
+++ GameStore.Application/Validation/CommentCreateValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using GameStore.Application.Models;
+
+namespace GameStore.Application.Validation;
+
+public class CommentCreateValidator : AbstractValidator<CommentCreateModel>
+{
+    public CommentCreateValidator()
+    {
+        RuleFor(c => c.Body)
+            .NotEmpty()
+            .WithMessage("Comment can't be empty.")
+            .MaximumLength(250)
+            .WithMessage("Comment length can't exceed 250 characters.");
+
+        RuleFor(c => c.GameId)
+            .GreaterThan(0)
+            .WithMessage("Incorrect game id.");
+    }
+}
