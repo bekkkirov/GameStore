e199e30da5a58120cfdb7c89da1baf0a79e4a759
bekirov <vbekirovv@gmail.com>
2022-09-22 12:59:46 +0300

    Added services interfaces.

---
+++ GameStore.Application/Interfaces/ICommentService.cs
@@ -0,0 +1,10 @@
+﻿using GameStore.Application.Models;
+
+namespace GameStore.Application.Interfaces;
+
+public interface ICommentService
+{
+    Task<IEnumerable<CommentModel>> GetByGameKeyAsync(string key);
+
+    Task AddAsync(string userName, CommentCreateModel comment);
+}
---
+++ GameStore.Application/Interfaces/IGameService.cs
@@ -0,0 +1,22 @@
+﻿using GameStore.Application.Models;
+
+namespace GameStore.Application.Interfaces;
+
+public interface IGameService
+{
+    Task<IEnumerable<GameModel>> GetAllAsync();
+
+    Task<GameModel> GetByKeyAsync(string key);
+
+    Task<IEnumerable<GameModel>> GetByGenreAsync(int genreId);
+
+    Task<IEnumerable<GameModel>> GetByPlatformTypeAsync(int platformId);
+
+    Task<GameModel> AddAsync(GameCreateModel game);
+
+    Task UpdateAsync(int gameId, GameCreateModel game);
+
+    Task DeleteAsync(int gameId);
+
+    MemoryStream DownloadGame(string key);
+}
