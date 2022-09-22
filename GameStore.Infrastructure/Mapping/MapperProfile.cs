8f8dfd4c1d374f5c4948c2784ffa334f791e2578
bekirov <vbekirovv@gmail.com>
2022-09-22 12:56:53 +0300

    Added AutoMapper, implemented mapper profile.

---
+++ GameStore.Infrastructure/Mapping/MapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GameStore.Application.Models;
+using GameStore.Domain.Entities;
+
+namespace GameStore.Infrastructure.Mapping;
+
+public class MapperProfile : Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<Genre, GenreModel>().ReverseMap();
+
+        CreateMap<PlatformType, PlatformTypeModel>().ReverseMap();
+
+        CreateMap<Comment, CommentModel>()
+            .ForMember(d => d.Name, 
+                opt => opt.MapFrom(src => src.Author.FirstName + src.Author.LastName));
+        CreateMap<CommentCreateModel, Comment>();
+
+        CreateMap<Game, GameModel>();
+        CreateMap<GameCreateModel, Game>()
+            .ForMember(d => d.Genres,
+                opt => opt.MapFrom(src => src.GenreIds.Select(id => new Genre() {Id = id})))
+            .ForMember(d => d.PlatformTypes, opt => opt.MapFrom(src => src.PlatformIds.Select(id => new PlatformType() {Id = id})));
+    }
+}
