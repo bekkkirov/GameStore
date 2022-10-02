642c501b12c030a71eea140e530fdc1d4c8f251f
bekirov <vbekirovv@gmail.com>
2022-10-02 14:04:23 +0300

    Added new functionality to the image repository.

--- GameStore.Application/Persistence/Repositories/IImageRepository.cs
+++ GameStore.Application/Persistence/Repositories/IImageRepository.cs
@@ -4,5 +4,7 @@ namespace GameStore.Application.Persistence.Repositories;
 
 public interface IImageRepository : IRepository<Image>
 {
-    
+    Task<Image> GetByPublicIdAsync(string publicId);
+
+    Task DeleteByPublicIdAsync(string publicId);
 }
--- GameStore.Infrastructure/Persistence/Repositories/ImageRepository.cs
+++ GameStore.Infrastructure/Persistence/Repositories/ImageRepository.cs
@@ -1,5 +1,7 @@
-﻿using GameStore.Application.Persistence.Repositories;
+﻿using GameStore.Application.Exceptions;
+using GameStore.Application.Persistence.Repositories;
 using GameStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Infrastructure.Persistence.Repositories;
 
@@ -7,5 +9,22 @@ public class ImageRepository : BaseRepository<Image>, IImageRepository
 {
     public ImageRepository(GameStoreContext context) : base(context)
     {
+    }
+
+    public async Task<Image> GetByPublicIdAsync(string publicId)
+    {
+        return await _dbSet.FirstOrDefaultAsync(i => i.PublicId == publicId);
+    }
+
+    public async Task DeleteByPublicIdAsync(string publicId)
+    {
+        var image = await _dbSet.FirstOrDefaultAsync(i => i.PublicId == publicId);
+
+        if (image is null)
+        {
+            throw new NotFoundException("Image with specified public id not found.");
+        }
+
+        _dbSet.Remove(image);
     }
 }
