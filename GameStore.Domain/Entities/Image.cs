3af74fe07355027a34f2856d2ab6668b321723f0
bekirov <vbekirovv@gmail.com>
2022-10-02 10:51:05 +0300

    Added image entity. Added image property to the game entity.

---
+++ GameStore.Domain/Entities/Image.cs
@@ -0,0 +1,13 @@
+﻿using GameStore.Domain.Common;
+
+namespace GameStore.Domain.Entities;
+
+public class Image : BaseEntity
+{
+    public string Url { get; set; }
+
+    public string PublicId { get; set; }
+
+    public int? GameId { get; set; }
+    public Game Game { get; set; }
+}
