8dcc131903663a9e8e85aa8112df0ea8cbd1c780
bekirov <vbekirovv@gmail.com>
2022-10-02 14:02:08 +0300

    Added configuration for the image entity.

---
+++ GameStore.Infrastructure/Persistence/Configurations/ImageConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using GameStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameStore.Infrastructure.Persistence.Configurations;
+
+public class ImageConfiguration : IEntityTypeConfiguration<Image>
+{
+    public void Configure(EntityTypeBuilder<Image> builder)
+    {
+        builder.Property(i => i.Url)
+               .IsRequired();
+
+        builder.Property(i => i.PublicId)
+               .IsRequired();
+
+        builder.HasIndex(i => i.PublicId)
+               .IsUnique();
+    }
+}
---
+++ GameStore.Infrastructure/Persistence/Migrations/20221002105919_ImageConfiguration.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.Infrastructure.Persistence.Migrations
+{
+    public partial class ImageConfiguration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Images",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicId",
+                table: "Images",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_PublicId",
+                table: "Images",
+                column: "PublicId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Images_PublicId",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Images",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicId",
+                table: "Images",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
