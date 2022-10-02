b09586e2f4575ce37d18259a234efd7971d792b5
bekirov <vbekirovv@gmail.com>
2022-10-02 10:59:27 +0300

    Added CloudinaryOptions.

---
+++ GameStore.Application/Options/CloudinaryOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStore.Application.Options;
+
+public class CloudinaryOptions
+{
+    public string CloudName { get; set; }
+
+    public string ApiKey { get; set; }
+
+    public string ApiSecret { get; set; }
+}
