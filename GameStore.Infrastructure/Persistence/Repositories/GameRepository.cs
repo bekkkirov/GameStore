6bcbf0a024ebfe53c528f43f1e614f4af88bde96
bekirov <vbekirovv@gmail.com>
2022-10-03 14:43:07 +0300

    Now game repository loads game image.

--- GameStore.Infrastructure/Persistence/Repositories/GameRepository.cs
+++ GameStore.Infrastructure/Persistence/Repositories/GameRepository.cs
@@ -12,31 +12,35 @@ public class GameRepository : BaseRepository<Game>, IGameRepository
 
     public async Task<IEnumerable<Game>> GetWithPlatformsAndGenres()
     {
-        return await _dbSet.Include(g => g.Genres)
+        return await _dbSet.Include(g => g.Image)
+                           .Include(g => g.Genres)
                            .Include(g => g.PlatformTypes)
                            .ToListAsync();
     }
 
     public async Task<Game> GetByKeyAsync(string key)
     {
-        return await _dbSet.Include(g => g.Genres)
+        return await _dbSet.Include(g => g.Image)
+                           .Include(g => g.Genres)
                            .Include(g => g.PlatformTypes)
                            .FirstOrDefaultAsync(g => g.Key == key);
     } 
 
     public async Task<IEnumerable<Game>> GetByGenreAsync(int genreId)
     {
-        return await _dbSet.Where(g => g.Genres.Any(g => g.Id == genreId))
+        return await _dbSet.Include(g => g.Image)
                            .Include(g => g.Genres)
                            .Include(g => g.PlatformTypes)
+                           .Where(g => g.Genres.Any(g => g.Id == genreId))
                            .ToListAsync();
     }
 
     public async Task<IEnumerable<Game>> GetByPlatformAsync(int platformId)
     {
-        return await _dbSet.Where(g => g.PlatformTypes.Any(pt => pt.Id == platformId))
+        return await _dbSet.Include(g => g.Image)
                            .Include(g => g.Genres)
                            .Include(g => g.PlatformTypes)
+                           .Where(g => g.PlatformTypes.Any(pt => pt.Id == platformId))
                            .ToListAsync();
     }
 }
