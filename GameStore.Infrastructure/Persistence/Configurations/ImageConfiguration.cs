a30824cea389b3621ce81d4c76a1b5c2a2327b83
bekirov <vbekirovv@gmail.com>
2022-10-04 11:20:35 +0300

    Updated image configuration.

--- GameStore.Infrastructure/Persistence/Configurations/ImageConfiguration.cs
+++ GameStore.Infrastructure/Persistence/Configurations/ImageConfiguration.cs
@@ -9,9 +9,11 @@ public class ImageConfiguration : IEntityTypeConfiguration<Image>
     public void Configure(EntityTypeBuilder<Image> builder)
     {
         builder.Property(i => i.Url)
+               .HasMaxLength(150)
                .IsRequired();
 
         builder.Property(i => i.PublicId)
+               .HasMaxLength(50)
                .IsRequired();
 
         builder.HasIndex(i => i.PublicId)
---
+++ GameStore.Infrastructure/Persistence/Migrations/20221004080917_UpdatedImageConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.Infrastructure.Persistence.Migrations
+{
+    public partial class UpdatedImageConfiguration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Images",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicId",
+                table: "Images",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Images",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicId",
+                table: "Images",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
