1d16b4f555aea3e7e52064a06b33b2f6202129b6
bekirov <vbekirovv@gmail.com>
2022-10-09 19:04:35 +0300

    Added identity.

---
+++ GameStore.Infrastructure/Identity/Persistence/IdentityContext.cs
@@ -0,0 +1,13 @@
+﻿using GameStore.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Infrastructure.Identity.Persistence;
+
+public class IdentityContext : IdentityDbContext<UserIdentity, UserRole, int>
+{
+    public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+    {
+        
+    }
+}
