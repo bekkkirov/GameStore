ba335d0f087d1a1ce14f60c13837dc8008c8b55b
bekirov <vbekirovv@gmail.com>
2022-10-12 19:12:40 +0300

    Updated user repository, user model. Added user service.

--- GameStore.Application/Models/UserModel.cs
+++ GameStore.Application/Models/UserModel.cs
@@ -4,11 +4,9 @@ public class UserModel
 {
     public string UserName { get; set; }
 
-    public string Email { get; set; }
-
     public string FirstName { get; set; }
 
     public string LastName { get; set; }
 
-    public ImageModel Image { get; set; }
+    public ImageModel ProfileImage { get; set; }
 }
---
+++ GameStore.Infrastructure/Services/UserService.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using GameStore.Application.Exceptions;
+using GameStore.Application.Interfaces;
+using GameStore.Application.Models;
+using GameStore.Application.Persistence;
+using Microsoft.AspNetCore.Http;
+
+namespace GameStore.Infrastructure.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IImageService _imageService;
+    private readonly IMapper _mapper;
+
+    public UserService(IUnitOfWork unitOfWork, IImageService imageService, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _imageService = imageService;
+        _mapper = mapper;
+    }
+
+    public async Task<UserModel> GetUserInfoAsync(string userName)
+    {
+        var user = await _unitOfWork.UserRepository.GetByUserNameAsync(userName);
+
+        if (user is null)
+        {
+            throw new NotFoundException("User with specified username doesn't exist.");
+        }
+
+        return _mapper.Map<UserModel>(user);
+    }
+
+    public async Task<ImageModel> SetProfileImageAsync(string userName, IFormFile image)
+    {
+        var createdImage = await _imageService.AddAsync(image);
+
+        var user = await _unitOfWork.UserRepository.GetByUserNameAsync(userName);
+
+        if (user is null)
+        {
+            throw new NotFoundException("User with specified username doesn't exist.");
+        }
+
+        if (user.ProfileImage is not null)
+        {
+            await _imageService.DeleteAsync(user.ProfileImage.PublicId);
+        }
+
+        createdImage.UserId = user.Id;
+
+        _unitOfWork.ImageRepository.Update(createdImage);
+        await _unitOfWork.SaveChangesAsync();
+
+        return _mapper.Map<ImageModel>(createdImage);
+    }
+}
