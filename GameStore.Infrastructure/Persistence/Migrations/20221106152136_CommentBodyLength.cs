e10a9c9fac31a32a194ac374785195da90c20efb
bekirov <vbekirovv@gmail.com>
2022-11-06 17:22:46 +0200

    Changed maximum length of the comment body.

---
+++ GameStore.Infrastructure/Persistence/Migrations/20221106152136_CommentBodyLength.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.Infrastructure.Persistence.Migrations
+{
+    public partial class CommentBodyLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Comments",
+                type: "nvarchar(600)",
+                maxLength: 600,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMarkedForDeletion",
+                table: "Comments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRoot",
+                table: "Comments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMarkedForDeletion",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "IsRoot",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Comments",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(600)",
+                oldMaxLength: 600);
+        }
+    }
+}
