f2ad61c0bd86a776eaa57d8e8e27732a489d572e
bekirov <vbekirovv@gmail.com>
2022-12-04 14:42:50 +0200

    Updated app component.

--- GameStore.Client/src/app/app.component.ts
+++ GameStore.Client/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProgressSpinnerService} from "./services/progress-spinner.service";
+import {AuthService} from "./services/auth.service";
+import {UserService} from "./services/user.service";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     title = 'GameStore';
 
-    constructor(public progressSpinnerService: ProgressSpinnerService) {
+    constructor(public progressSpinnerService: ProgressSpinnerService,
+                private authService: AuthService,
+                private userService: UserService) {
+    }
+
+    ngOnInit(): void {
+        let token = localStorage.getItem('authToken');
+
+        if(token) {
+            this.userService.getCurrentUser().subscribe(result => this.authService.setCurrentUser(result));
+        }
     }
 }
