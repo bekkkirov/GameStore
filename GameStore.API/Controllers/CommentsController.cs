076e6779d0d803cf6fbb1c71951e0463c629e557
bekirov <vbekirovv@gmail.com>
2022-12-04 21:25:45 +0200

    Removed unused injection.

--- GameStore.API/Controllers/CommentsController.cs
+++ GameStore.API/Controllers/CommentsController.cs
@@ -14,7 +14,7 @@ public class CommentsController : ControllerBase
 {
     private readonly ICommentService _commentService;
 
-    public CommentsController(ICommentService commentService, ICurrentUserService currentUserService)
+    public CommentsController(ICommentService commentService)
     {
         _commentService = commentService;
     }
