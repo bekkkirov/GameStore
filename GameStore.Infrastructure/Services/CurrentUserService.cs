557cc8da9d5e0f3f06a19b79c31983f349285ddd
bekirov <vbekirovv@gmail.com>
2022-12-04 21:00:55 +0200

    Implemented current user service.

---
+++ GameStore.Infrastructure/Services/CurrentUserService.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using GameStore.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace GameStore.Infrastructure.Services;
+
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly ClaimsPrincipal _currentUser;
+
+    public CurrentUserService(IHttpContextAccessor context)
+    {
+        _currentUser = context.HttpContext?.User;
+    }
+
+    public string GetUsername()
+    {
+        return _currentUser.FindFirstValue(ClaimTypes.Name);
+    }
+}
