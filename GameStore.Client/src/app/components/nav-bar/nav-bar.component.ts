a5060c8ec7ebe4da8391340fff8da929791ad87d
bekirov <vbekirovv@gmail.com>
2022-12-21 18:48:44 +0200

    Updated nav bar component.

--- GameStore.Client/src/app/components/nav-bar/nav-bar.component.ts
+++ GameStore.Client/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {SignInComponent} from "../sign-in/sign-in.component";
 import {AuthService} from "../../services/auth.service";
 import {User} from "../../models/user";
+import {UserService} from "../../services/user.service";
+import {EMPTY, mergeMap} from "rxjs";
 
 @Component({
     selector: 'app-nav-bar',
@@ -10,8 +12,11 @@ import {User} from "../../models/user";
     styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+    @ViewChild('imageInput') imageInput;
+
     constructor(private authDialog: MatDialog,
-                public authService: AuthService) {
+                public authService: AuthService,
+                private userService: UserService) {
     }
 
     ngOnInit(): void {
@@ -28,5 +33,21 @@ export class NavBarComponent implements OnInit {
 
     logout() {
         this.authService.logout();
+    }
+
+    changeProfileImage() {
+        let image = this.imageInput.nativeElement.files[0];
+
+        this.userService.setProfileImage(image).subscribe(image => {
+            let user: User;
+            this.authService.currentUser$.subscribe(u =>
+            {
+                user = u;
+                user.profileImage = image;
+                this.authService.setCurrentUser(user);
+            });
+
+
+        });
     }
 }
