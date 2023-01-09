7487b5efd62554e63621f66e88546050ca864a03
bekirov <vbekirovv@gmail.com>
2023-01-09 12:25:14 +0200

    Added entity configurations.

---
+++ GameStore.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using GameStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameStore.Infrastructure.Persistence.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.Property(o => o.FirstName)
+               .IsRequired()
+               .HasMaxLength(20);
+
+        builder.Property(o => o.LastName)
+               .IsRequired()
+               .HasMaxLength(20);
+
+        builder.Property(o => o.Phone)
+               .HasMaxLength(15)
+               .IsRequired();
+
+        builder.Property(o => o.Email)
+               .IsRequired()
+               .HasMaxLength(30);
+
+        builder.Property(o => o.Comment)
+               .HasMaxLength(600);
+
+        builder.Property(o => o.PaymentType)
+               .HasConversion(
+                   v => v.ToString(),
+                   v => (PaymentType)Enum.Parse(typeof(PaymentType), v));
+
+        builder.HasOne(o => o.User)
+               .WithMany(u => u.Orders)
+               .HasForeignKey(o => o.UserId);
+    }
+}
---
+++ GameStore.Infrastructure/Persistence/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using GameStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameStore.Infrastructure.Persistence.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.Property(i => i.Amount)
+               .IsRequired();
+
+        builder.HasOne(i => i.Order)
+               .WithMany(o => o.Items)
+               .HasForeignKey(i => i.OrderId);
+
+        builder.HasOne(i => i.Game)
+               .WithMany()
+               .HasForeignKey(i => i.GameId);
+    }
+}
