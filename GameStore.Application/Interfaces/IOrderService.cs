955940d807e251c10741e8f3f4ca3693d73ed810
bekirov <vbekirovv@gmail.com>
2023-01-10 22:05:54 +0200

    Implemented order service.

---
+++ GameStore.Application/Interfaces/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using GameStore.Application.Models;
+
+namespace GameStore.Application.Interfaces;
+
+public interface IOrderService
+{
+    Task<OrderModel> GetByIdAsync(int id);
+
+    Task<OrderModel> AddOrderAsync(CreateOrderModel order);
+}
