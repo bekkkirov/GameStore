e9839573f9bca395453589cc2c3f89bbe2d97930
bekirov <vbekirovv@gmail.com>
2023-01-10 22:04:49 +0200

    Added dtos.

---
+++ GameStore.Application/Models/OrderItemModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GameStore.Application.Models;
+
+public class OrderItemModel
+{
+    public int GameId { get; set; }
+
+    public int Amount { get; set; }
+}
---
+++ GameStore.Application/Models/OrderModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GameStore.Application.Models;
+
+public class OrderModel
+{
+    public int Id { get; set; }
+
+    public List<OrderItemModel> Items { get; set; } = new List<OrderItemModel>();
+}
