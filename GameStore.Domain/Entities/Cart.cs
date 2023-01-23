6049d3221ec9f41460676474e4fae599bc0e8759
bekirov <vbekirovv@gmail.com>
2023-01-23 13:24:39 +0200

    Updated cart entity.

--- GameStore.Domain/Entities/Cart.cs
+++ GameStore.Domain/Entities/Cart.cs
@@ -7,7 +7,7 @@ public class Cart : BaseEntity
     public int UserId { get; set; }
     public User User { get; set; }
 
-    public Order Order { get; set; }
+    public bool IsOrdered { get; set; }
 
     public List<OrderItem> Items { get; set; } = new List<OrderItem>();
 }
